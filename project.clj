d8638c5d561e4d8e8927def6658fec54d5dd2f89
Hans Huebner <hans.huebner@gmail.com>
2015-04-14 06:14:29 +0200

    use sudharsh's clj-oauth2 fork

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [stuarth/clj-oauth2 "0.3.2"]
+                 [sudharsh/clj-oauth2 "0.5.3"]
                  [ring "1.3.2"]
                  [compojure "1.3.3"]
                  [org.apache.httpcomponents/httpclient "4.3.5"]
