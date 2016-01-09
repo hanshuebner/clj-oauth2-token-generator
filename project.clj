a746a02208a7801ae2d6537344ce743028ba43dc
Hans Huebner <hans.huebner@gmail.com>
2016-01-09 08:08:17 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "Server to generate Google OAuth2 tokens"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [sudharsh/clj-oauth2 "0.5.3"]
-                 [ring "1.3.2"]
-                 [compojure "1.3.3"]
-                 [org.apache.httpcomponents/httpclient "4.3.5"]
-                 [clj-http "1.1.0"]]
+                 [ring "1.4.0"]
+                 [compojure "1.4.0"]
+                 [org.apache.httpcomponents/httpclient "4.5.1"]
+                 [clj-http "2.0.0"]]
   :main clj-oauth2-token-generator.core)
